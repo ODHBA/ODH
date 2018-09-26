7118ac1f2589abd08f3c50936a3c708566bae5ff
pzaninelli <pablozaninelli@gmail.com>
2018-09-26 18:21:46 -0300

    Modificaciones Pablo

--- 02_estadisticos_economia_social.R
+++ 02_estadisticos_economia_social.R
@@ -19,18 +19,18 @@
 suppressPackageStartupMessages(library(dplyr))
 library(magrittr)
 library(ggplot2)
-library(broom)
-library(extrafont)
+# library(broom)
+# library(extrafont)
 
 #' ## DIRECTORIOS
-DATOS <- "/home/noelia/MEGAsync/ODH/datos_economia_social/"
-CENSOS <- "/home/noelia/MEGAsync/ODH/datos_censos/"
-FIGURAS <- "/home/noelia/MEGAsync/ODH/figuras/"
+DATOS <- "datos_economia_social/"
+CENSOS <- "datos_censos/"
+FIGURAS <- "figuras/"
 
 #' ## FICHEROS ENTRADA
 tabla2 <- read.csv(paste0(DATOS, "10_economia_social_tabla2.csv"))
 presupuesto <-  read.csv(paste0(DATOS, "10_economia_social_tabla3.csv"))
-censo <- read.csv(paste0(CENSOS, "10_censo_provincia_sexo_elecc15_argentina.csv"))
+censo <- read.csv(paste0(CENSOS, "censo_nombrecambiado.csv"))
 
 censo[5] <- "Capital_Federal"
 
@@ -58,15 +58,19 @@ ellas_hacen <- tabla2 %>%
 #' https://www.r-graph-gallery.com/48-grouped-barplot-with-ggplot2/
 #' http://felixfan.github.io/ggplot2-remove-grid-background-margin/
 #' 
+a <- c("gold3","gold3","blue","blue","blue","blue","gold3","blue","blue","gold3",
+"blue","blue","gold3","violet","violet","violet","blue","blue","blue","blue",
+"violet","violet","blue","blue")
+#' 
 # Stacked Percent
 geh <- ggplot(ellas_hacen, aes(fill=Periodo, y=Titulares, x=Provincia)) + 
   geom_bar( stat="identity", position="fill")
 geh <- geh + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
                panel.background = element_blank(), axis.line = element_line(colour = "black"),
-          axis.text.x = element_text(angle = 90, hjust = 1))
+          axis.text.x = element_text(angle = 90, hjust = 1, colour = a))
 print(geh + ggtitle("Programa 'Ellas Hacen' 2015-2017"))
 
-#'ggsave(paste0(FIGURAS, "economia_social_fig1.png"), geh)
+ggsave(paste0(FIGURAS, "economia_social_fig1.png"), geh)
 
 
 #' ## Elegir las bases de datoS "AT"
@@ -79,14 +83,35 @@ argentina_trabaja <- tabla2 %>%
 #' https://www.r-graph-gallery.com/48-grouped-barplot-with-ggplot2/
 #' http://felixfan.github.io/ggplot2-remove-grid-background-margin/
 #' 
+
+#' 
 # Stacked Percent
 gat <- ggplot(argentina_trabaja, aes(fill=Periodo, y=Titulares, x=Provincia)) + 
   geom_bar( stat="identity", position="fill")
 gat <- gat + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
                    panel.background = element_blank(), axis.line = element_line(colour = "black"),
-                   axis.text.x = element_text(angle = 90, hjust = 1))
+                   axis.text.x = element_text(angle = 90, hjust = 1, colour = a))
 print(gat + ggtitle("Programa 'Argentina Trabaja' 2015-2017"))
 
 ggsave(paste0(FIGURAS, "economia_social_fig2.png"), gat)
 
 #' ## Normalizar con respecto a los 
+#' cambio los nombres de las provincias en el data frame de 'censo'
+censo$Provincia <- c("BUENOS_AIRES","CATAMARCA","CHACO","CHUBUT",
+                  "CAPITAL_FEDERAL","CORRIENTES","CORDOBA",
+                  "ENTRE_RIOS","FORMOSA","JUJUY","LA_PAMPA",
+                  "LA_RIOJA","MENDOZA","MISIONES",
+                  "NEUQUEN","RIO_NEGRO","SALTA","SAN_JUAN","SAN_LUIS","SANTA_CRUZ",
+                  "SANTA_FE","SANTIAGO_DEL_ESTERO","TIERRA_DEL_FUEGO","TUCUMAN")
+#' junto ambos data frames por provincias
+df4 <- merge(censo,tabla2,by = "Provincia")
+EH <- df4 %>% filter(Programa == "EH")
+
+geh <- ggplot(EH, aes(fill=as.character(Periodo), y=(Titulares/Mujeres)*100, x=Provincia)) + 
+  geom_bar(stat = "identity") + scale_fill_discrete(name="Años")
+geh <- geh + ylab("%") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"),
+        axis.text.x = element_text(angle = 90, hjust = 1, color=a))
+print(geh + ggtitle("Programa 'Ellas Hacen' 2015-2017 normalizado con Censo 2010"))
+ggsave(paste0(FIGURAS,"EH_bar_norm.png"),geh,dpi = 600)
